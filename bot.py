1b16f40eaac3dadf17b4f2485ed7efb191a90a74
3RD <seva052010@gmail.com>
2024-01-07 01:08:58 +0300

    Базовый функционал веток, комманды /тикет /добавить

--- bot.py
+++ bot.py
@@ -3,7 +3,6 @@ from discord import app_commands, Color
 from discord.ext import commands
 from dotenv import load_dotenv, dotenv_values
 from icecream import ic
-from config import colors
 from random import randint, choice
 
 #Загрузка переменных окружения из файла .env
@@ -18,28 +17,67 @@ client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
 #Подключение к базе данных
-conn = sqlite3.connect("CycleSite.db")
-cursor = conn.cursor()
-cursor.close()
-conn.commit()
-conn.close()
+# conn = sqlite3.connect("CycleSite.db")
+# cursor = conn.cursor()
+# cursor.close()
+# conn.commit()
+# conn.close()
+
+class ticket_launcher(discord.ui.View):
+
+    def __init__(self) -> None:
+        super().__init__(timeout=None) 
+    
+    @discord.ui.button(label="Открыть тикет", style=discord.ButtonStyle.green, custom_id="open_ticket")
+    async def open_ticket(self, interaction: discord.Interaction, button: discord.ui.Button):
+        overwrites = {
+            interaction.guild.default_role: discord.PermissionOverwrite(view_channel=False),
+            interaction.user: discord.PermissionOverwrite(view_channel=True, send_messages=True, attach_files=True),
+            interaction.guild.me: discord.PermissionOverwrite(view_channel=True, send_messages=True, read_message_history=True)
+        }
+        channel = await interaction.guild.create_text_channel(f"Тикет-для-{interaction.user.name}", overwrites=overwrites, category=interaction.channel.category)
+        embed = discord.Embed(title="Тикет открыт!", color=config.colors.info)
+        embed.set_author(name=interaction.user.name, icon_url=interaction.user.display_avatar)
+        await channel.send(embed=embed, view = ticket_operator())
+        embed = discord.Embed(title="Тикет открыт", description=f"В канале {channel.mention}", color=config.colors.info)
+        await interaction.response.send_message(embed=embed, ephemeral=True)
+
+class ticket_operator(discord.ui.View):
+
+    def __init__(self) -> None:
+        super().__init__(timeout=None)
+
+    @discord.ui.button(label="Закрыть тикет", style=discord.ButtonStyle.red, custom_id="close_ticket")
+    async def close(self, interaction, button):
+        embed = discord.Embed(title="Вы уверены что хотите закрыть тикет?", description=f"Удаление автоматически отменится через {config.auto_cancel_time} секунд", color=config.colors.info)
+        await interaction.response.send_message(embed = embed, view = confirm_deletion(), ephemeral = True, delete_after = config.auto_cancel_time)
+
+class confirm_deletion(discord.ui.View):
+
+    def __init__(self) -> None:
+        super().__init__(timeout=config.auto_cancel_time)
+
+    @discord.ui.button(label="Удалить", style=discord.ButtonStyle.red)
+    async def delete(self, interaction, button):
+        await interaction.channel.delete()
+        
+
+@client.event
+async def setup_hook():
+    global synced
+    synced = False
+    client.add_view(ticket_launcher())
+    client.add_view(ticket_operator())
 
 @client.event
 async def on_ready():
-    print(f'{client.user.name} подключён к серверу!    \n{round(client.latency * 1000)}ms')
+    global synced
+    print('')
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='на тебя <3'))
-
-#Синхронизация дерева комманд. Можно сделать тоже самое в on_ready ----------------
-@tree.command(name="синхронизация", description="Только для инженеров", guild=discord.Object(id=guild))
-async def sync(intrct):
-    if intrct.user.id in config.bot_engineers:
+    if not synced:
         await tree.sync(guild=discord.Object(id=guild))
-        embed = discord.Embed(title="Комманды синхронизированы!", color=config.colors.success)
-        embed.set_author(name=intrct.user.display_name, icon_url=intrct.user.display_avatar)
-        await intrct.response.send_message(embed=embed)
-    else:
-        embed = discord.Embed(title="Эта комманда не для тебя", color=config.colors.danger)
-        await intrct.response.send_message(embed=embed)
+        synced = True
+    print(f'{client.user.name} подключён к серверу!    \n{round(client.latency * 1000)}ms')
 
 #Пинг бота по slash-комманде ----------------
 @tree.command(name="пинг", description="Пингани бота!", guild=discord.Object(id=guild))
@@ -55,17 +93,22 @@ async def on_message(message):
     if randint(0, 5) == 1:
         await message.add_reaction(choice(message.guild.emojis))
 
-#Безопасное выключение бота ----------------
-@tree.command(name="выключение", description="Только для инженеров", guild=discord.Object(id=guild))
-async def shutdown(intrct):
-    if intrct.user.id in config.bot_engineers:
-        embed = discord.Embed(title="Бот выключается...", color=config.colors.warning)
-        embed.set_author(name=intrct.user.display_name, icon_url=intrct.user.display_avatar)
-        await intrct.response.send_message(embed=embed)
+@tree.command(name="тикет", description="Запускает систему тикетов в текущей категории!", guild=discord.Object(id=guild))
+async def ticketing(intrct):
+    embed = discord.Embed(title="PLACEHOLDER", description="PLACEHOLDER", color=config.colors.info)
+    client.add_view(ticket_launcher())
+    await intrct.channel.send(embed=embed, view=ticket_launcher())
+    await intrct.response.send_message("Система тикетов была успешно (или почти) запущена", ephemeral=True)
+
+@tree.command(name="добавить", description="Добавляет пользователя в тикет", guild=discord.Object(id=guild))
+@app_commands.describe(user="Пользователь для добавления в тикет")
+@discord.app_commands.checks.has_role(config.admin_role)
+async def add_user(intrct, user: discord.User):
+    if "тикет-для-" in intrct.channel.name:
+        await intrct.channel.set_permissions(user, read_messages=True, send_messages=True, attach_files=True)
+        embed = discord.Embed(title=f"Пользователь добавлен", description=f"{user.mention} был добавлен в тикет {intrct.user.mention}", color=config.colors.info)
+        await intrct.response.send_message(embed=embed, ephemeral=False)
     else:
-        embed = discord.Embed(title="Эта комманда не для тебя", color=config.colors.danger)
-        await intrct.response.send_message(embed=embed)
-
-
+        await intrct.response.send_message("Это не тикет!", ephemeral=True)
 
 client.run(token)
--- config.py
+++ config.py
@@ -1,9 +1,12 @@
-bot_engineers = [554281270260006914, 949708119334854658]
+bot_engineers = [554281270260006914, 949708119334854658] #Управляющие ботом
+admin_role = 1188023192510545992 #Роль Администратора
 
-class colors:
-    info = 0x5c5eff
+class colors: #Цвета дизайна бота
+    info = 0x5c5eff #Совпадает с цветом роли
     danger = 0xda4957
     success = 0x4fff4d
     warning = 0xf9c327
     white = 0xFFFFFF
     black = 0x000000
+
+auto_cancel_time = 15 #Время автоматической отмены подтверждения в секундах
